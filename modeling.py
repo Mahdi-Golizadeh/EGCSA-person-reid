891db5130536c3df164301a9070ec268853a9e64
Mahdi-Golizadeh <mastergolizadeh@gmail.com>
2024-03-26 08:58:10 +0330

    major update

--- modeling.py
+++ modeling.py
@@ -681,7 +681,7 @@ class GAT(torch.nn.Module):
         x21 = self.conv_sp_1(x21.permute(-4, -1, -3, -2))
         out = (self.elu1(x11) * x + self.elu2(x21) * x)
         x11 = self.conv_ch_2(out)
-        conv_layer = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(h, w), device= MODEL_DEVICE)
+        conv_layer = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=(h, w), device= MODEL_DEVICE)
         x21 = conv_layer(out)
         return self.sig1(x11) * x + self.sig2(x21) * x
 
